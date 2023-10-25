2e07c280462a1ecdd3b5ac407acb9a61cd49531f
Diana Atanasova <dianaa@vmware.com>
2023-10-25 19:03:51 +0300

    Initial commit
    Add Readme file
    Add python script & notebook
    
    TODO: Sync both python script and the notebook. They currently have differences in the code.
    TODO: Update Pipeline steps descriptions within the notebook
    
    Signed-off-by: Diana Atanasova <dianaa@vmware.com>

--- README.md
+++ README.md
@@ -1,1 +1,125 @@
-# vml-2023
+# Serve large language models (LLMs) with custom prompt tuning configuration using Kubeflow Pipelines
+
+This repository  idemonstrates how Kubeflow could be levaraged for prompt tuning foundational LLM and serving tuned models. 
+
+This notebook walks throught:
+1. Train the prompt tuning configuration against Hugging Face open source models.
+2. Publish a trained configuration to Hugging Face.
+3. Serve the prompt tuning configuration along with HuggingFace open source large language models (LLMs).
+3. Automate prompt tuning processing with Kubeflow Pipelines.
+4. Automate the prompt tuning LLM serving process by using Kubeflow Pipelines. 
+
+- [Prerequisites](#prerequisites)
+
+- [Kubeflow Installation](#kubeflow-installation)
+- [Create PodDefault resource](#create-poddefault-resource)
+- [Access Kubeflow UI](#access-kubeflow-ui)
+
+- [KServe Modelmesh Installation](#modelmesh-installation)
+
+- [Import Notebook into Kubeflow JupyterLab](#import-notebook-into-kubeflow-jupyterlab)
+- [Run through the Notebook](#run-through-the-notebook)
+- [Track the workflow using Kubeflow UI](#track-the-workflow-using-kubeflow-ui)
+
+## Prerequisites
+
+To successfully run the example provided in this repository, Kubeflow cluster and KServe ModelMesh need to brought up. Before installing them, you must have the following dependencies installed in local environment.
+- Python 3.9+
+- Docker
+- Kubectl
+- Kustomize 5.0.0
+- Install Kubernetes locally (could be done via `kind`, `minikube`, `docker desktop for Mac`)
+
+## Kubeflow Installation
+
+This example has been tested with Kubeflow version 1.8. 
+
+To install Kubeflow, clone the [Manifests repo](https://github.com/kubeflow/manifests) and run the installation using kustomize
+```bash
+git clone --branch v1.8-branch https://github.com/kubeflow/manifests.git && cd manifests
+while ! kustomize build example | awk '!/well-defined/' | kubectl apply -f -; do echo "Retrying to apply resources"; sleep 10; done
+```
+
+Before continuing, make sure to wait for all pods in `kubeflow` namespace to become ready
+```bash
+kubectl -n kubeflow wait --for=condition=Ready pods --all --timeout=1200s
+```
+
+## KServe Modelmesh Installation
+
+ModelMesh Serving is the Controller for managing ModelMesh, a general-purpose model serving management/routing layer. The instructions for installing it are provided on the bellows. For more detailed information on how to get started, check our this [link](https://github.com/kserve/modelmesh-serving/blob/main/docs/quickstart.md)
+
+1. Clone Modelmesh serving repository
+```bash
+RELEASE="release-0.11"
+git clone -b $RELEASE --depth 1 --single-branch https://github.com/kserve/modelmesh-serving.git
+cd modelmesh-serving
+```
+2. Create a namespace called `modelmesh-serving` to deploy ModelMesh to.
+```bash
+kubectl create namespace modelmesh-serving
+./scripts/install.sh --namespace modelmesh-serving --quickstart
+```
+
+## Adjust service account permissions
+Give our current service account `kubeflow-user-example-com:default-editor` permissions to manipulate `inferenceservices` and `servingruntimes` within `modelmesh-serving` namespace: 
+
+```bash
+kubectl create clusterrole servicemesh-editor --verb=get,create,delete,list,watch,patch --resource=inferenceservices,servingruntime
+kubectl create rolebinding servicemesh-editor --serviceaccount=kubeflow-user-example-com:default-editor --clusterrole=servicemesh-editor -n modelmesh-serving
+```
+
+## Create k8s secret with your Hugging Face account token
+
+Modify and execute the following command to store your Hugging Face account token as a secret in the Kubeflow  cluster. This secret is used by the pipeline to publish the prompt tuning configuration to Hugging Face after the training. You can obtain the Hugging Face account token with WRITE permission on their [website](https://huggingface.co/settings/tokens).
+```
+kubectl create secret generic huggingface-secret --from-literal='token=<HuggingFace_WRITE_Token>' -n kubeflow
+```
+
+## Create PodDefault resource
+Create `PodDefault` to inject `ServiceAccount` token volumne into your Pods. Once created and configured correctly with your notebook, this will allow all pods created by the notebook to access kubeflow pipelines.
+
+```
+kubectl apply -f - <<EOF
+apiVersion: kubeflow.org/v1alpha1
+kind: PodDefault
+metadata:
+  name: access-kf-pipeline
+  namespace: kubeflow-user-example-com
+spec:
+  desc: Allow access to KFP
+  selector:
+    matchLabels:
+      access-kf-pipeline: "true"
+  volumeMounts:
+    - mountPath: /var/run/secrets/kubeflow/pipelines
+      name: volume-kf-pipeline-token
+      readOnly: true
+  volumes:
+    - name: volume-kf-pipeline-token
+      projected:
+        sources:
+          - serviceAccountToken:
+              path: token
+              expirationSeconds: 7200
+              audience: pipelines.kubeflow.org
+  env:
+    - name: KF_PIPELINES_SA_TOKEN_PATH
+      value: /var/run/secrets/kubeflow/pipelines/token
+EOF
+```
+
+## Access Kubeflow UI
+
+After successful installation of Kubeflow, use `port-forward` to expose the `istio-ingressgateway` service by running the following
+  ```
+  kubectl port-forward svc/istio-ingressgateway -n istio-system 8080:80
+  ```
+
+Navigate to [localhost:8080](http://localhost:8080/) and login using `user@example.com` and password `12341234`
+
+## Import Notebook into Kubeflow JupyterLab
+
+Once logged in to the Kubeflow dashboard, navigate to "Notebooks" to create a new `JupyterLab` notebook with `kubeflownotebookswg/jupyter-tensorflow-full:v1.8.0-rc.0` image and configuration "Allow access to Kubeflow Pipelines" enabled (available in "Advanced options").
+
+After notebook is running, `connect` to the notebook and upload [this]() notebook.
---
+++ prompt-tuning-pipeline.py
@@ -0,0 +1,367 @@
+import kfp
+import kfp.dsl as dsl
+from kfp.dsl import component
+from kfp.compiler import Compiler
+import kfp.components as comp
+
+hf_name=""
+hf_token=""
+
+peft_model_server_image="quay.io/aipipeline/peft-model-server:latest"
+modelmesh_namespace="modelmesh-serving"
+modelmesh_servicename="modelmesh-serving"
+pipeline_out_file="llm-prompt_tuning_pipeline.yaml"
+kserv_component="https://raw.githubusercontent.com/kubeflow/pipelines/release-2.0.1/components/kserve/component.yaml"
+
+@component(
+    packages_to_install=["peft", "transformers", "datasets", "torch", "datasets", "tqdm"],
+    base_image='python:3.10'
+)
+def prompt_tuning_bloom(peft_model_publish_id: str, model_name_or_path: str, num_epochs: int):
+    from transformers import AutoModelForCausalLM, AutoTokenizer, default_data_collator, get_linear_schedule_with_warmup
+    from peft import get_peft_config, get_peft_model, PromptTuningInit, PromptTuningConfig, TaskType, PeftType
+    import torch
+    from datasets import load_dataset
+    import os
+    from torch.utils.data import DataLoader
+    from tqdm import tqdm
+
+    peft_config = PromptTuningConfig(
+        task_type=TaskType.CAUSAL_LM,
+        prompt_tuning_init=PromptTuningInit.TEXT,
+        num_virtual_tokens=8,
+        prompt_tuning_init_text="Classify if the tweet is a complaint or not:",
+        tokenizer_name_or_path=model_name_or_path,
+    )
+
+    dataset_name = "twitter_complaints"
+    text_column = "Tweet text"
+    label_column = "text_label"
+    max_length = 64
+    lr = 3e-2
+    batch_size = 8
+
+    dataset = load_dataset("ought/raft", dataset_name)
+    dataset["train"][0]
+
+    classes = [k.replace("_", " ") for k in dataset["train"].features["Label"].names]
+    dataset = dataset.map(
+        lambda x: {"text_label": [classes[label] for label in x["Label"]]},
+        batched=True,
+        num_proc=1,
+    )
+
+    tokenizer = AutoTokenizer.from_pretrained(model_name_or_path)
+    if tokenizer.pad_token_id is None:
+        tokenizer.pad_token_id = tokenizer.eos_token_id
+
+    def preprocess_function(examples):
+        batch_size = len(examples[text_column])
+        inputs = [f"{text_column} : {x} Label : " for x in examples[text_column]]
+        targets = [str(x) for x in examples[label_column]]
+        model_inputs = tokenizer(inputs)
+        labels = tokenizer(targets)
+        for i in range(batch_size):
+            sample_input_ids = model_inputs["input_ids"][i]
+            label_input_ids = labels["input_ids"][i] + [tokenizer.pad_token_id]
+            model_inputs["input_ids"][i] = sample_input_ids + label_input_ids
+            labels["input_ids"][i] = [-100] * len(sample_input_ids) + label_input_ids
+            model_inputs["attention_mask"][i] = [1] * len(model_inputs["input_ids"][i])
+        for i in range(batch_size):
+            sample_input_ids = model_inputs["input_ids"][i]
+            label_input_ids = labels["input_ids"][i]
+            model_inputs["input_ids"][i] = [tokenizer.pad_token_id] * (
+                max_length - len(sample_input_ids)
+            ) + sample_input_ids
+            model_inputs["attention_mask"][i] = [0] * (max_length - len(sample_input_ids)) + model_inputs[
+                "attention_mask"
+            ][i]
+            labels["input_ids"][i] = [-100] * (max_length - len(sample_input_ids)) + label_input_ids
+            model_inputs["input_ids"][i] = torch.tensor(model_inputs["input_ids"][i][:max_length])
+            model_inputs["attention_mask"][i] = torch.tensor(model_inputs["attention_mask"][i][:max_length])
+            labels["input_ids"][i] = torch.tensor(labels["input_ids"][i][:max_length])
+        model_inputs["labels"] = labels["input_ids"]
+        return model_inputs
+    
+    processed_datasets = dataset.map(
+        preprocess_function,
+        batched=True,
+        num_proc=1,
+        remove_columns=dataset["train"].column_names,
+        load_from_cache_file=False,
+        desc="Running tokenizer on dataset",
+    )
+
+    train_dataset = processed_datasets["train"]
+    eval_dataset = processed_datasets["train"]
+
+
+    train_dataloader = DataLoader(
+        train_dataset, shuffle=True, collate_fn=default_data_collator, batch_size=batch_size, pin_memory=False
+    )
+    eval_dataloader = DataLoader(eval_dataset, collate_fn=default_data_collator, batch_size=batch_size, pin_memory=False)
+
+    model = AutoModelForCausalLM.from_pretrained(model_name_or_path)
+    model = get_peft_model(model, peft_config)
+    print(model.print_trainable_parameters())
+
+    optimizer = torch.optim.AdamW(model.parameters(), lr=lr)
+    lr_scheduler = get_linear_schedule_with_warmup(
+        optimizer=optimizer,
+        num_warmup_steps=0,
+        num_training_steps=(len(train_dataloader) * num_epochs),
+    )
+
+    for epoch in range(num_epochs):
+        model.train()
+        total_loss = 0
+        for step, batch in enumerate(tqdm(train_dataloader)):
+            batch = {k: v for k, v in batch.items()}
+            outputs = model(**batch)
+            loss = outputs.loss
+            total_loss += loss.detach().float()
+            loss.backward()
+            optimizer.step()
+            lr_scheduler.step()
+            optimizer.zero_grad()
+
+        model.eval()
+        eval_loss = 0
+        eval_preds = []
+        for step, batch in enumerate(tqdm(eval_dataloader)):
+            batch = {k: v for k, v in batch.items()}
+            with torch.no_grad():
+                outputs = model(**batch)
+            loss = outputs.loss
+            eval_loss += loss.detach().float()
+            eval_preds.extend(
+                tokenizer.batch_decode(torch.argmax(outputs.logits, -1).detach().cpu().numpy(), skip_special_tokens=True)
+            )
+
+        eval_epoch_loss = eval_loss / len(eval_dataloader)
+        eval_ppl = torch.exp(eval_epoch_loss)
+        train_epoch_loss = total_loss / len(train_dataloader)
+        train_ppl = torch.exp(train_epoch_loss)
+        print("epoch=%s: train_ppl=%s train_epoch_loss=%s eval_ppl=%s eval_epoch_loss=%s" % (epoch, train_ppl, train_epoch_loss, eval_ppl, eval_epoch_loss))
+
+    from huggingface_hub import login
+    token = os.environ.get("HUGGINGFACE_TOKEN")
+
+    login(token=token)
+
+    peft_model_id = peft_model_publish_id
+    model.save_pretrained("output_dir") 
+    model.push_to_hub(peft_model_id, use_auth_token=True)
+
+@component(
+    packages_to_install=["kubernetes"],
+    base_image='python:3.10'
+)
+def deploy_modelmesh_custom_runtime(peft_model_publish_id: str, model_name_or_path: str, server_name: str, namespace: str, image: str):
+    import kubernetes.config as k8s_config
+    import kubernetes.client as k8s_client
+    from kubernetes.client.exceptions import ApiException
+
+    def create_custom_object(group, version, namespace, plural, manifest):
+        cfg = k8s_client.Configuration()
+        cfg.verify_ssl=False
+        cfg.host = "https://kubernetes.default.svc"
+        cfg.api_key_prefix['authorization'] = 'Bearer'
+        cfg.ssl_ca_cert = '/var/run/secrets/kubernetes.io/serviceaccount/ca.crt'
+        with open("/var/run/secrets/kubernetes.io/serviceaccount/token") as f:
+            lines = f.readlines()
+            for l in lines:
+                cfg.api_key['authorization'] = "{}".format(l)
+                break
+        with k8s_client.ApiClient(cfg) as api_client:
+            capi = k8s_client.CustomObjectsApi(api_client)
+            try:
+                res = capi.create_namespaced_custom_object(group=group,
+                                                           version=version, namespace=namespace,
+                                                           plural=plural, body=manifest)
+            except ApiException as e:
+                # object already exists
+                if e.status != 409:
+                    raise
+    custom_runtime_manifest = {
+        "apiVersion": "serving.kserve.io/v1alpha1",
+        "kind": "ServingRuntime",
+        "metadata": {
+            "name": "{}-server".format(server_name),
+            "namespace": namespace
+        },
+        "spec": {
+            "supportedModelFormats": [
+            {
+                "name": "peft-model",
+                "version": "1",
+                "autoSelect": True
+            }
+            ],
+            "multiModel": True,
+            "grpcDataEndpoint": "port:8001",
+            "grpcEndpoint": "port:8085",
+            "containers": [
+            {
+                "name": "mlserver",
+                "image": image,
+                "env": [
+                {
+                    "name": "MLSERVER_MODELS_DIR",
+                    "value": "/models/_mlserver_models/"
+                },
+                {
+                    "name": "MLSERVER_GRPC_PORT",
+                    "value": "8001"
+                },
+                {
+                    "name": "MLSERVER_HTTP_PORT",
+                    "value": "8002"
+                },
+                {
+                    "name": "MLSERVER_LOAD_MODELS_AT_STARTUP",
+                    "value": "true"
+                },
+                {
+                    "name": "MLSERVER_MODEL_NAME",
+                    "value": "peft-model"
+                },
+                {
+                    "name": "MLSERVER_HOST",
+                    "value": "127.0.0.1"
+                },
+                {
+                    "name": "MLSERVER_GRPC_MAX_MESSAGE_LENGTH",
+                    "value": "-1"
+                },
+                {
+                    "name": "PRETRAINED_MODEL_PATH",
+                    "value": model_name_or_path
+                },
+                {
+                    "name": "PEFT_MODEL_ID",
+                    "value": peft_model_publish_id
+                }
+                ],
+                "resources": {
+                "requests": {
+                    "cpu": "500m",
+                    "memory": "4Gi"
+                },
+                "limits": {
+                    "cpu": "5",
+                    "memory": "5Gi"
+                }
+                }
+            }
+            ],
+            "builtInAdapter": {
+            "serverType": "mlserver",
+            "runtimeManagementPort": 8001,
+            "memBufferBytes": 134217728,
+            "modelLoadingTimeoutMillis": 90000
+            }
+        }
+    }
+    create_custom_object(group="serving.kserve.io", version="v1alpha1",
+                         namespace=namespace, plural="servingruntimes",
+                         manifest=custom_runtime_manifest)
+
+@component(
+    base_image='python:3.10'
+)
+def inference_svc(model_name: str, namespace: str) -> str :
+
+    inference_service = '''
+apiVersion: serving.kserve.io/v1beta1
+kind: InferenceService
+metadata:
+  name: {}
+  namespace: {}
+  annotations:
+    serving.kserve.io/deploymentMode: ModelMesh
+spec:
+  predictor:
+    model:
+      modelFormat:
+        name: peft-model
+      runtime: {}-server
+      storage:
+        key: localMinIO
+        path: sklearn/mnist-svm.joblib
+'''.format(model_name, namespace, model_name)
+
+    return inference_service
+
+@component(
+    packages_to_install=["transformers", "peft", "torch", "requests"],
+    base_image='python:3.10'
+)
+def test_modelmesh_model(service: str,  namespace: str, model_name: str, input_tweet: str):
+    import requests
+    import base64
+    import json
+
+    url = "http://%s.%s:8008/v2/models/%s/infer" % (service, namespace, model_name)
+    input_json = {
+        "inputs": [
+            {
+            "name": "content",
+            "shape": [1],
+            "datatype": "BYTES",
+            "data": [input_tweet]
+            }
+        ]
+    }
+
+    x = requests.post(url, json = input_json)
+
+    print(x.text)
+    respond_dict = json.loads(x.text)
+    inference_result = respond_dict["outputs"][0]["data"][0]
+    base64_bytes = inference_result.encode("ascii")
+  
+    string_bytes = base64.b64decode(base64_bytes)
+    inference_result = string_bytes.decode("ascii")
+    print("inference_result: %s " % inference_result)
+
+# Define your pipeline function
+@dsl.pipeline(
+    name="Serving LLM with Prompt tuning",
+    description="A Pipeline for Serving Prompt Tuning LLMs on Modelmesh"
+)
+def prompt_tuning_pipeline(
+    peft_model_publish_id: str = hf_name+"/bloomz-560m_PROMPT_TUNING_CAUSAL_LM",
+    model_name_or_path: str = "bigscience/bloomz-560m",
+    model_name: str = "vml-demo",
+    input_tweet: str = "@nationalgridus I have no water and the bill is current and paid. Can you do something about this?",
+    test_served_llm_model: str ="true",
+    num_epochs: int = 50
+):
+    prompt_tuning_llm = prompt_tuning_bloom( peft_model_publish_id=peft_model_publish_id, 
+                                             model_name_or_path=model_name_or_path,
+                                             num_epochs=num_epochs)
+    prompt_tuning_llm.set_env_variable('HUGGINGFACE_TOKEN', hf_token)
+    deploy_modelmesh_custom_runtime_task = deploy_modelmesh_custom_runtime(peft_model_publish_id=peft_model_publish_id, 
+                                                                           model_name_or_path=model_name_or_path,
+                                                                           server_name=model_name, namespace=modelmesh_namespace,
+                                                                           image=peft_model_server_image)
+    deploy_modelmesh_custom_runtime_task.after(prompt_tuning_llm)
+
+    inference_svc_task = inference_svc(model_name=model_name, namespace=modelmesh_namespace)
+    inference_svc_task.after(deploy_modelmesh_custom_runtime_task)
+    inference_svc_task.set_caching_options(False)
+    
+    kserve_launcher_op = comp.load_component_from_url(kserv_component)
+    serve_llm_with_peft_task = kserve_launcher_op(action="apply", 
+                                                  inferenceservice_yaml=inference_svc_task.output)
+    serve_llm_with_peft_task.after(inference_svc_task)
+    serve_llm_with_peft_task.set_caching_options(False)
+
+    with dsl.If(test_served_llm_model == 'true'):
+        test_modelmesh_model_task = test_modelmesh_model(service=modelmesh_servicename, namespace=modelmesh_namespace,
+                                                         model_name=model_name, input_tweet=input_tweet).after(serve_llm_with_peft_task)
+        test_modelmesh_model_task.set_caching_options(False)
+
+# Compile the pipeline
+Compiler().compile(prompt_tuning_pipeline, pipeline_out_file)
